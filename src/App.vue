ac00e40754cf1bf1efea07bc04a6af86f93cda19
lcds90 <lcds90@gmail.com>
2022-08-05 00:20:24 -0300

    chore: initial project 🐏

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import TheNavbarVue from "./components/TheNavbar.vue";
+</script>
+<template>
+  <TheNavbarVue></TheNavbarVue>
+
+  <main></main>
+</template>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <div class="logo">
+        <img />
+      </div>
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/contact">Contact</router-link></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
